2f1c07d84abed4eb53b9cfd61d19381c3dad1929
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-04-09 19:52:15 +0530

    update

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div style={{overflow:'hidden'}}>
+    <div  style={{overflow:'hidden'}}>
       <div className='container-fluid  bg-[#16154d]'>
         <div className='grid gap-4 sm:grid-cols-3  md:grid-cols-5 lg:grid-cols-12  lg:gap-32   lg:ml-64 p-2 '>
 
@@ -28,7 +28,7 @@ const Navbar = () => {
       </div>
 
 
-      <nav className="bg-[#FA7B1D] mb-10">
+      <nav className=" bg-[#FA7B1D] mb-10">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
