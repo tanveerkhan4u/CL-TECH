bcecd4987c590eeb6056c799ba97f5094ad0e8ef
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-04-27 18:36:40 +0530

    update

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
 
         </div>
       </div>
-      <div className='  container-fluid grid sm:grid-cols-3  md:grid-cols-3 lg:grid-cols-3  bg-yellow-50'>
-        <img src={download2} className=' w-32 h-auto  lg:ml-60 '  alt='CL-TECH' />
+      <div className='  container   grid sm:grid-rows-3  md:grid-rows-3 lg:grid-rows-3  bg-yellow-50'>
+        <img src={download2} className=' w-32 h-auto   '  alt='CL-TECH' />
         <div className='text-4xl  mt-5'>
           <h1 className='font-serif font-bold text-emerald-700 hover:text-emerald-500  mr-8 text-center'>CL-TECH INSTITUTE OF INDIA</h1>
         </div>
